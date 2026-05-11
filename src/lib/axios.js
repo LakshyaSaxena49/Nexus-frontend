0eedbe4806c8f12c9e280c4a7c346d747bd655c2
Lakshya Saxena <lakshyasaxena49@gmail.com>
2026-05-12 00:21:02 +0530

    fix: production issue

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "https://nexus-backend-ovam.onrender.com/api",
+
   withCredentials: true,
 });
